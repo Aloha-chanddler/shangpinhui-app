e4fe114194cbdbd05df7eb3f27dd9eaf1fe44696
LiuChang <lcc1875@163.com>
2022-07-06 18:21:48 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <appHeader />
+    我是根组件
+    <appFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import appHeader from "./components/Header";
+import appFooter from "./components/Footer";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { appHeader, appFooter },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+.footer {
+  background-color: #eaeaea;
+
+  .footer-container {
+    width: 1200px;
+    margin: 0 auto;
+    padding: 0 15px;
+
+    .footerList {
+      padding: 20px;
+      border-bottom: 1px solid #e4e1e1;
+      border-top: 1px solid #e4e1e1;
+      overflow: hidden;
+      padding-left: 40px;
+
+      .footerItem {
+        width: 16.6666667%;
+        float: left;
+
+        h4 {
+          font-size: 14px;
+        }
+
+        .footerItemCon {
+          li {
+            line-height: 18px;
+          }
+        }
+
+        &:last-child img {
+          width: 121px;
+        }
+      }
+    }
+
+    .copyright {
+      padding: 20px;
+
+      .helpLink {
+        text-align: center;
+
+        li {
+          display: inline;
+
+          .space {
+            border-left: 1px solid #666;
+            width: 1px;
+            height: 13px;
+            background: #666;
+            margin: 8px 10px;
+          }
+        }
+      }
+
+      p {
+        margin: 10px 0;
+        text-align: center;
+      }
+    }
+  }
 }
 </style>
